20d5eb814e62de0633e0dfdf59027ccc3c1dcad4
MIKU_N <sakura.rain.snow@gmail.com>
2022-06-11 22:27:57 +0800

    fix cmake loss

--- include/php-modules.sh
+++ include/php-modules.sh
@@ -125,7 +125,7 @@ install_php_depends(){
     #Remove cmake install
     elif check_sys packageManager yum; then
         yum_depends=(
-            autoconf patch m4 bison bzip2-devel pam-devel gmp-devel libicu-devel
+            cmake autoconf patch m4 bison bzip2-devel pam-devel gmp-devel libicu-devel
             curl-devel pcre-devel libtool-libs libtool-ltdl-devel libwebp-devel libXpm-devel
             libvpx-devel libjpeg-devel libpng-devel freetype-devel oniguruma-devel
             aspell-devel enchant-devel readline-devel libtidy-devel sqlite-devel
